814b9d15c881c3cd38c0828bd87a5a584762730d
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2023-07-28 02:14:16 +0200

    xml is now read with digger and more in line with other components

--- src/io/mqtt/MqttPool.java
+++ src/io/mqtt/MqttPool.java
@@ -5,9 +5,8 @@ import io.telnet.TelnetCodes;
 import das.Commandable;
 import util.data.RealtimeValues;
 import org.tinylog.Logger;
-import org.w3c.dom.Element;
+import util.xml.XMLdigger;
 import util.xml.XMLfab;
-import util.xml.XMLtools;
 import worker.Datagram;
 
 import java.nio.file.Path;
@@ -40,16 +39,6 @@ public class MqttPool implements Commandable, MqttWriting {
         return false;
     }
     /**
-     * Get The @see MQTTWorker based on the given id
-     *
-     * @param id The id of the MQTT worker requested
-     * @return The worder requested or null if not found
-     */
-    public Optional<MqttWorker> getMqttWorker(String id) {
-        return Optional.ofNullable( mqttWorkers.get(id) );
-    }
-
-    /**
      * Get a list of all the MQTTWorker id's
      *
      * @return Set of all the id's
@@ -72,86 +61,6 @@ public class MqttPool implements Commandable, MqttWriting {
                 .add(worker.getSubscriptions("\r\n")));
         return join.toString();
     }
-
-    /**
-     * Adds a subscription to a certain MQTTWorker
-     *
-     * @param id    The id of the worker to add it to
-     * @param label The label associated wit the data, this will be given to @see
-     *              BaseWorker when data is recevied
-     * @param topic The topic to subscribe to
-     * @return True if a subscription was successfully added
-     */
-    public boolean addMQTTSubscription(String id, String label, String topic) {
-        MqttWorker worker = mqttWorkers.get(id);
-        if (worker == null)
-            return false;
-        return worker.addSubscription(topic, label);
-    }
-    public boolean addBroker( String id, String address, String defTopic){
-        var broker = new MqttWorker(address,defTopic,dQueue);
-        broker.setID(id);
-        mqttWorkers.put( id, broker);
-        return updateMQTTsettings(id);
-    }
-    /**
-     * Remove a subscription from a certain MQTTWorker
-     *
-     * @param id    The id of the worker
-     * @param topic The topic to remove
-     * @return True if it was removed, false if it wasn't either because not found
-     *         or no such worker
-     */
-    public boolean removeMQTTSubscription(String id, String topic) {
-        MqttWorker worker = mqttWorkers.get(id);
-        if (worker == null)
-            return false;
-        return worker.removeSubscription(topic);
-    }
-
-    /**
-     * Update the settings in the xml for a certain MQTTWorker based on id
-     *
-     * @param id The worker of which the settings need to be altered
-     * @return True if updated
-     */
-    public boolean updateMQTTsettings(String id) {
-        XMLfab fab = XMLfab.withRoot(settingsFile,"dcafs")
-                            .selectOrAddChildAsParent("mqtt")
-                            .down();
-
-        MqttWorker worker = mqttWorkers.get(id);
-        if (worker != null ){
-            worker.updateXMLsettings(fab, true);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Reload the settings for a certain MQTTWorker from the settings.xml
-     *
-     * @param id The worker for which the settings need to be reloaded
-     * @return True if this was successful
-     */
-    public boolean reloadMQTTsettings(String id) {
-        MqttWorker worker = mqttWorkers.get(id);
-        if (worker == null)
-            return false;
-
-        var mqttOpt = XMLtools.getFirstElementByTag( settingsFile, "mqtt");
-
-        if( mqttOpt.isEmpty())
-            return false;
-
-        for (Element broker : XMLtools.getChildElements(mqttOpt.get(), "broker")) {
-            if (XMLtools.getStringAttribute(broker, "id", "general").equals(id)) {
-                worker.readSettings(broker);
-                return true;
-            }
-        }
-        return false;
-    }
     /**
      * Reload the settings from the settings.xml
      *
@@ -159,16 +68,28 @@ public class MqttPool implements Commandable, MqttWriting {
      */
     public boolean readXMLsettings() {
 
-        var mqttOpt = XMLtools.getFirstElementByTag(settingsFile, "mqtt");
+        // Disconnect current ones if any
+        mqttWorkers.values().forEach(MqttWorker::disconnect);
+        mqttWorkers.clear(); // Clear to start fresh
 
-        if( mqttOpt.isEmpty())
+        var dig = XMLdigger.goIn(settingsFile,"dcafs","mqtt");
+        if( dig.isInvalid())
             return false;
 
-        for (Element broker : XMLtools.getChildElements(mqttOpt.get(), "broker")) {
-            String id = XMLtools.getStringAttribute(broker, "id", "general");
-            Logger.info("Adding MQTT broker called " + id);
-            mqttWorkers.put(id, new MqttWorker(broker, dQueue));
-        }
+        dig.digOut("broker").forEach( broker -> {
+            var id = broker.attr("id","general");
+            var addr = broker.peekAt("address").value("");
+            var clientid = broker.peekAt("clientid").value("");
+            var defTopic = broker.peekAt("defaulttopic").value("");
+
+            var worker = new MqttWorker(id,addr,clientid,defTopic);
+
+            broker.peekOut("subscribe").forEach( sub -> {
+                worker.addSubscription(sub.getTextContent(),sub.getAttribute("label"));
+            });
+            worker.applySettings();
+            mqttWorkers.put(id,worker);
+        });
         return true;
     }
 
@@ -205,6 +126,17 @@ public class MqttPool implements Commandable, MqttWriting {
                 case "brokers" -> {
                     return getMqttBrokersInfo();
                 }
+                case "reload" -> {
+                    if (readXMLsettings())
+                        return "Settings for " + cmds[0] + " reloaded.";
+                    return "! Failed to reload settings.";
+                }
+                case "test" -> {
+                    mqttWorkers.values().forEach( w -> {
+                        w.addWork("dice/d20","10");
+                    });
+                    return "Testing";
+                }
                 default -> {
                     var worker = mqttWorkers.get(cmds[0]);
                     if (worker == null)
@@ -219,7 +151,15 @@ public class MqttPool implements Commandable, MqttWriting {
         }else if( cmds[0].equalsIgnoreCase("addbroker")){
             if (cmds.length != 4)
                 return "! Wrong amount of arguments -> mqtt:addbroker,id,address,deftopic";
-            return addBroker(cmds[1], cmds[2], cmds[3]) ? "Broker added" : "! Failed to add broker";
+            if( mqttWorkers.containsKey(cmds[1]))
+                return "ID already in use";
+
+            var fab = XMLfab.withRoot(settingsFile,"dcafs","mqtt");
+            fab.addParentToRoot("broker").attr("id",cmds[1]); // make broker root
+            fab.addChild("address",cmds[2]);
+            fab.addChild("defaulttopic",cmds[3]);
+            readXMLsettings();
+            return "Broker added";
         }else{
             var worker = mqttWorkers.get(cmds[0]);
             if( worker == null)
@@ -240,15 +180,6 @@ public class MqttPool implements Commandable, MqttWriting {
                         return "Subscription removed, send 'mqtt:"+cmds[0]+",store' to save settings to xml";
                     return "! Failed to remove subscription, probably typo?";
                 }
-                case "reload" -> {
-                    if (reloadMQTTsettings(cmds[0]))
-                        return "Settings for " + cmds[0] + " reloaded.";
-                    return "! Failed to reload settings.";
-                }
-                case "store" -> {
-                    updateMQTTsettings(cmds[0]);
-                    return "Settings updated";
-                }
                 case "send" -> {
                     if (cmds.length != 3)
                         return "! Wrong amount of arguments -> mqtt:id,send,topic:value";
@@ -257,7 +188,7 @@ public class MqttPool implements Commandable, MqttWriting {
 
                     String[] topVal = cmds[2].split(":");
                     double val = rtvals.getReal(topVal[1], -999);
-                    worker.addWork(topVal[0], "" + val);
+                    worker.addWork(topVal[0], String.valueOf(val));
                     return "Data send to " + cmds[0];
                 }
                 default -> {
