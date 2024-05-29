7b7e29980e42b9c64ca13eb9a91c5f30a1ecaf7f
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2024-05-30 00:15:34 +0200

    Rewrote parts because to hard to understand and error prone

--- src/io/telnet/CommandLineInterface.java
+++ src/io/telnet/CommandLineInterface.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.StringJoiner;
 
 public class CommandLineInterface {
-    ByteBuf buffer = Unpooled.buffer(32,512);       // Buffer that holds the received data
+    ByteBuf buffer = Unpooled.buffer(9,512);       // Buffer that holds the received data
     private ArrayList<String> cmdHistory = new ArrayList<>(); // Buffer that holds the processed commands
     private int cmdHistoryIndex =-1; // Pointer to the last send historical cmd
 
@@ -32,15 +32,13 @@ public class CommandLineInterface {
      */
     public Optional<byte[]> receiveData(byte[] data ){
 
-        // Work with buffer
-        StringJoiner join = new StringJoiner(" ");
         byte[] rec=null;
         for( int a=0;a<data.length;a++ ){
             byte b = data[a];
             if( b == TelnetCodes.IAC ){ // Meaning start of command sequence
-                join.add( TelnetCodes.toReadableIAC(data[a++]))
-                        .add(TelnetCodes.toReadableIAC(data[a++]))
-                        .add(TelnetCodes.toReadableIAC(data[a]));
+                Logger.debug( TelnetCodes.toReadableIAC(data[a++]) +" - "
+                         + TelnetCodes.toReadableIAC(data[a++]) +" - "
+                         + TelnetCodes.toReadableIAC(data[a]) );
             }else if( b == 27 && data.length>1){ // Escape codes
                 a++;
                 Logger.debug("Received: "+ (char)b+ " or " +Integer.toString(b)+" "+Integer.toString(data[a])+Integer.toString(data[a+1]));
@@ -68,27 +66,21 @@ public class CommandLineInterface {
                 }
             }else if( b == '\n'){ //LF
                 writeByte(b); // echo LF
-            }else if( b == '\r' || b==19 || b==27 ) { // CR
+            }else if( b == '\r' || b==19 || b==27 ) { // CR or CTRL+S or ESC
                 writeByte((byte)13); // echo CR
-                if(b==27)
+                if(b==27) // 27 = ESC
                     insertByte((byte)27);
-                if(b==19 || b==27)
+                if( b==19 || b==27 ) // If CTRL+S or ESC, insert a 0
                     insertByte((byte)0);
-                int wi = buffer.writerIndex();
-                while( buffer.getByte(wi) != 0)
-                    wi++;
-                buffer.setIndex(0,wi);
-                rec = new byte[buffer.readableBytes()];
-                buffer.readBytes(rec);
-                buffer.clear();
-                buffer.setZero(0,wi);
-                String r = new String(rec);
-                cmdHistory.remove(r);
-                cmdHistory.add(r);
-                if( cmdHistory.size()>20)
-                    cmdHistory.remove(0);
-                cmdHistoryIndex = cmdHistory.size();
 
+                // When in the middle of altering text, the writer index isn't pointing to the last
+                // char, so find the index of the actual end
+                int wi = buffer.writerIndex();
+                if( buffer.capacity() > wi ) { // If writer index is at the end of the buffer, no use searching
+                    while (buffer.getByte(wi) != 0)
+                        wi++;
+                }
+                rec = readBuffer(wi);
             }else if( b == 126){// delete
                 writeString(TelnetCodes.CURSOR_RIGHT);
                 if( buffer.getByte(buffer.writerIndex()+1)!=0x00){
@@ -116,62 +108,99 @@ public class CommandLineInterface {
     }
 
     /**
+     * Read the cmd from the buffer and add it to the cmd history
+     * @param wi Index in the buffer of last useful data
+     * @return The read bytes
+     */
+    private byte[] readBuffer(int wi){
+        buffer.setIndex(0,wi); // Make sure it doesn't read more than 1 null?
+        var rec = new byte[buffer.readableBytes()];
+        buffer.readBytes(rec); // Copies the content to the array
+
+        // Reset the buffer
+        buffer.setZero(0,wi); // Clear everything that was read
+        buffer.clear(); // Clears reader and writer index
+
+        // Store the read cmd in the history and alter index
+        var r = new String(rec);
+        cmdHistory.remove(r); // Don't repeat cmds
+        cmdHistory.add(r);
+        if( cmdHistory.size()>20) // Limit it
+            cmdHistory.remove(0);
+        cmdHistoryIndex = cmdHistory.size(); // point to last item again
+
+        return rec;
+    }
+    /**
      * Shift the content of the buffer left starting with the current writerindex
      */
     private void shiftLeft(){
-        int old = buffer.writerIndex()-1; // index to the left
-        buffer.setIndex(buffer.readerIndex(),old); // Shift index to the left
-        writeString( TelnetCodes.CURSOR_LEFT ); // Shift cursor to the left
+        int start = buffer.writerIndex(); // Position to start from
+        buffer.writerIndex(start-1 ); // New position will be one to the left
 
-        while( buffer.getByte(buffer.writerIndex())!=0x00 ){
-            byte tw = buffer.getByte(buffer.writerIndex()+1);
-            buffer.writeByte(tw);
-            if( tw==0){
-                writeString(TelnetCodes.CURSOR_RIGHT);
-                writeByte((byte)127);
-            }else{
-                writeByte(tw);
-            }
+        int max = buffer.capacity()-1; // Highest allowed index
 
+        writeString( TelnetCodes.cursorLeft(1) ); // Move to position that will be removed
+        int moves = 1; // To count how many chars we need to shift back after wards
+        for( int a=start;a<=max;a++){ // Go through all chars in the buffer from current position
+            var moved = buffer.getByte(a);
+            buffer.setByte(a-1,moved);
+            if( moved == 0) // Nu use moving unused buffer, stop here
+                break;
+            writeByte(moved);
+            moves++;
         }
-        writeString(TelnetCodes.cursorLeft(buffer.writerIndex()-old-1));
-        buffer.setIndex(buffer.readerIndex(),old);
+        writeByte((byte)' '); // clear the last character
+        writeString( TelnetCodes.cursorLeft(moves) ); // Move back to where we came from
     }
-
     /**
      * Insert a byte at the current writerindex, shifting everything to the right of it ... to the right
      * @param b The byte to insert
      */
     private void insertByte( byte b ){
 
-        try {
-            if (buffer.writerIndex() > 2 && b == 0) {
-                if (buffer.getByte(buffer.writerIndex() - 1) == 0)
-                    return;
-            }
-
-            byte old = buffer.writableBytes()==0?0:buffer.getByte(buffer.writerIndex());
-            buffer.writeByte(b);
-
-            writeByte(b);
-            int offset = 0;
-            while (old != 0) { // Meaning there was text in front so move the rest to insert
-                byte ol = buffer.getByte(buffer.writerIndex());
-                buffer.writeByte(old);
-
-                writeByte(old);
-                old = ol;
-                offset++;
-            }
-            if (offset != 0) {
-                buffer.setIndex(buffer.readerIndex(), buffer.writerIndex() - offset);
-                writeString(TelnetCodes.cursorLeft(offset));
-            }
-        }catch( Exception e ){
-            Logger.error(e);
+        if (buffer.writerIndex() > 2 && b == 0) {
+            if (buffer.getByte(buffer.writerIndex() - 1) == 0)
+                return;
         }
-    }
 
+        int start = buffer.writerIndex(); // Position to start from
+
+        int max = buffer.capacity()-1; // Highest allowed index
+        int end = 0;
+
+        // Figure out where the data stops
+        while( buffer.getByte(end)!=0 && end < max )
+            end++;
+
+        // If the end is lower than the start, this is just appending
+        if( end <= start ) {
+            buffer.writeByte(b);
+            writeByte(b);
+            return;
+        }
+        // If we get here, it means stuff needs to be shifted to the right
+        if( end == max ){ // Meaning if we shift things we go out of bonds, so add to it
+            buffer.writerIndex(end+1); // Need to add it to the end
+            buffer.writeByte(buffer.getByte(end)); // Creates space for last char
+            buffer.writerIndex(start); // Restore it
+            end++;
+        }
+
+        // Move the rest
+        for( int a=end; a > start;a--){
+            var moved = buffer.getByte(a-1);
+            buffer.setByte( a,moved );
+        }
+        // Now do the insert
+        buffer.writeByte(b);
+
+        // Finally fix what the user sees
+        for( int a=start;a<=end;a++){
+            writeByte( buffer.getByte(a) );
+        }
+        writeString( TelnetCodes.cursorLeft(end-start) );
+    }
     /**
      * Send the historical command referenced to by the current histindex value and alter this value
      * @param adj The alteration to be applied to histIndex
@@ -193,7 +222,7 @@ public class CommandLineInterface {
         writeString("\r>" + cmdHistory.get(cmdHistoryIndex));//Move cursor and send history
         writeString(TelnetCodes.CLEAR_LINE_END); // clear the rest of the line
         buffer.clear(); // reset the reader and writer index
-        buffer.writeBytes(cmdHistory.get(cmdHistoryIndex).getBytes()); // fill the buffer
+        buffer.writeBytes(cmdHistory.get(cmdHistoryIndex).getBytes()); // fill the buffer with history
         try {
             buffer.setZero(buffer.writerIndex(), buffer.writableBytes() ); // clear the rest of the buffer
         }catch(Exception e){
