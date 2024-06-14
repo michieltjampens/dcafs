fbe39aa06634d0136ee9f3e405b31ab73cb63863
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2024-06-14 20:49:03 +0200

    cleanup (javadoc etc)

--- src/util/tools/Tools.java
+++ src/util/tools/Tools.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * A collection of various often used small methods that do a variety of usefull
+ * A collection of various often used small methods that do a variety of useful
  * things
  * 
  * @author Michiel T'Jampens
@@ -29,7 +29,7 @@ public class Tools {
 
     /* ********************************* D O U B L E ********************************************* */
     /**
-     * More robust way of parsing strings to double then the standard
+     * More robust way of parsing strings to double than the standard
      * Double.parseDouble method and return a chosen value on error Removes: space
      * ',' '\n' '\r'
      * 
@@ -67,48 +67,13 @@ public class Tools {
         BigDecimal bd = BigDecimal.valueOf(r);
         return bd.setScale(decimalPlace, RoundingMode.HALF_UP).doubleValue();
     }
-
-    /**
-     * Rounds a double to a certain amount of digits after the comma
-     * 
-     * @param r            The double to round
-     * @param decimalPlace The amount of digits
-     * @return The rounded double
-     */
-    public static String fixedLengthDouble(double r, int decimalPlace) {
-        if (Double.isInfinite(r) || Double.isNaN(r) || decimalPlace < 0)
-            return "-999";
-        BigDecimal bd = BigDecimal.valueOf(r);
-        double d = bd.setScale(decimalPlace, RoundingMode.HALF_UP).doubleValue();
-        StringBuilder l = new StringBuilder("" + d);
-        while ((l.length() - l.indexOf(".") - 1) < decimalPlace) {
-            l.append("0");
-        }
-        return l.toString();
-    }
-
-    /**
-     * Parse an element of an array of strings to a double
-     * 
-     * @param array The array containing the strings
-     * @param index The index of the element
-     * @param error The value to return if the parsing fails
-     * @return The parsed string
-     */
-    public static double parseDoubleArray(String[] array, int index, double error) {
-        if (array.length > index && index != -1) {
-            return parseDouble(array[index], error);
-        }
-        return error;
-    }
-
     /* ******************************* I N T E G E R  ************************************************************ */
     /**
-     * More robust way of parsing strings to integer then the standard
+     * More robust way of parsing strings to integer than the standard
      * Integer.parseInteger method and return a chosen value on error Removes: space
      * ',' '\n' '\r'
      * 
-     * @param number The string integer to parse, if starts with 0x, it's considered
+     * @param number The string integer to parse. If starts with 0x, it's considered
      *               hex
      * @param error  The integer to return if something went wrong
      * @return The parsed integer if successful or the chosen error value.
@@ -125,10 +90,24 @@ public class Tools {
             return error;
         }
     }
+
+    /**
+     * Check if the given string is parsable as a bool.
+     * @param value The value to check.
+     * @return True if it's a valid bool.
+     */
     public static boolean validBool( String value ){
         var valid = new String[]{"yes", "no", "true", "false", "1", "0"};
         return Arrays.asList(valid).contains(value.toLowerCase().trim());
     }
+
+    /**
+     * Parses a value to a boolean, returning the error value if conversion fails.
+     *
+     * @param value The value to parse.
+     * @param error The boolean value to return on error.
+     * @return The parsed value or error if failed.
+     */
     public static boolean parseBool( String value, boolean error){
         value=value.toLowerCase().trim();
         if( value.equals("yes")||value.equals("true")||value.equals("1")||value.equals("on")||value.equals("high"))
@@ -139,25 +118,18 @@ public class Tools {
             Logger.warn("No valid value received to convert to bool: "+value);
         return error;
     }
-    public static int toUnsigned(byte b) {
-        int a = b;
+
+    /**
+     * Convert a signed byte to an unsigned integer.
+     * @param val The byte to convert
+     * @return The unsigned equivalent of the given value.
+     */
+    public static int toUnsigned(byte val) {
+        int a = val;
         return a < 0 ? a + 256 : a;
     }
-    public static int toUnsignedWord(int b) {
-        int a = b;
-        return a < 0 ? a + 65536 : a;
-    }
-    /**
-     * Adds zeros to the front of an integer till it has the specified length
-     * @param nr the integer to alter
-     * @param length the requested length
-     * @return the altered integer as a string
-     */
-    public static String addLeadingZeros(int nr, int length) {
-        StringBuilder res = new StringBuilder("" + nr);
-        while (res.length() < length)
-            res.insert(0, "0");
-        return res.toString();
+    public static int toUnsignedWord(int val) {
+        return val < 0 ? val + 65536 : val;
     }
     /**
      * Adds zeros to the front of an integer till it has the specified length
@@ -172,6 +144,12 @@ public class Tools {
         return res.toString();
     }
     /* ************************************** S T R I N G ******************************************************** */
+
+    /**
+     * Check if the end of the string is a null.
+     * @param txt The string to check.
+     * @return True if the last character is null.
+     */
     public static boolean isNullEnded( String txt ){
         var bytes = txt.getBytes();
         if( bytes.length==0)
@@ -190,77 +168,7 @@ public class Tools {
                              .replace("nextion","\\x7F\\x7F\\x7F");
         return fromEscapedStringToBytes(delimiter);
 	}
-    /**
-     * Method to remove the values from the text that aren't in the ascii range
-     * 
-     * @param text The text to check for non-ascii
-     * @return The cleaned result
-     */
-    public static String removeNonAscii(String text) {
-        char[] a = text.toCharArray();
-        StringBuilder result = new StringBuilder();
-        for (int x = 0; x < a.length; x++) {
-            if ((int) a[x] < 128) {
-                result.append(a[x]);
-            } else {
-                int start = x > 50 ? (x - 50) : 0;
-                int cnt = a.length > (start + 100) ? start + 100 : a.length - start - 1;
-                Logger.error("Bad data within: " + String.copyValueOf(a, start, cnt));
-            }
-        }
-        return result.toString();
-    }
-
-    /**
-     * Remove backspaces from a string
-     * 
-     * @param data The string to remove backspaces from
-     * @return The cleaned data
-     */
-    public static String cleanBackspace(String data) {
-        char[] b = data.toCharArray();
-
-        for (int a = 1; a < b.length; a++) {
-            if ((int) b[a] == 8 || (int) b[a] == 127) {
-                b[a] = '@';
-            }
-        }
-        String d = new String(b);
-        while (d.contains("@")) {
-            int x = d.indexOf("@");
-            d = d.substring(0, x - 1) + d.substring(x + 1);
-        }
-        return d;
-    }
-
     /* ************************** * H E X A D E C I M A L ********************************************************* */
-    /**
-     * Converts a array of characters to a space separated string of hexadecimals
-     * (0x00 0x01)
-     * 
-     * @param data The array to parse
-     * @return The hex string
-     */
-    public static String fromCharToHexString(char[] data) {
-        StringJoiner join = new StringJoiner(" ");
-
-        for (char a : data) {
-            String hex = Integer.toHexString(a).toUpperCase();
-            join.add((hex.length() == 1 ? "0x0" : "0x") + hex);
-        }
-        return join.toString();
-    }
-
-    /**
-     * Convert a string to the hexadecimal representation of the characters
-     * 
-     * @param data The string to convert
-     * @return Hexadecimal version of the data
-     */
-    public static String fromAsciiToHex(String data) {
-        return fromCharToHexString(data.toCharArray());
-    }
-
     /**
      * Converts a array of bytes to a space separated string of hexadecimals (0x00
      * 0x01)
@@ -328,25 +236,6 @@ public class Tools {
         }
         return result;
     }
-    public static String[] extractNumbers(String txt ){
-        return Pattern.compile("[0-9]+\\.?[0-9]*")
-                .matcher(txt)
-                .results()
-                .map(MatchResult::group)
-                .toArray(String[]::new);
-    }
-    public static ArrayList<String> extractMatches(String txt, String regex ){
-        try {
-            return Pattern.compile(regex).matcher(txt)
-                    .results()
-                    .map(MatchResult::group)
-                    .collect(Collectors.toCollection(ArrayList::new));
-        }catch( Exception e){
-            Logger.error(e);
-        }
-        return new ArrayList<>();
-    }
-
     /**
      * Search the given txt for regex matches and alter those with the value but with append and or prepend
      * @param txt The txt to check/alter
@@ -464,6 +353,13 @@ public class Tools {
         }
         return out.toByteArray();
     }
+    /**
+     * Parses a string with number in ascii format to a byte.
+     *
+     * @param base    The base of these number (fe 2, 10 or 16)
+     * @param number The value to parse
+     * @return The resulting byte if parsed or empty optional if something went wrong.
+     */
     public static Optional<Byte> fromBaseToByte( int base, String number ){
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
@@ -488,51 +384,6 @@ public class Tools {
         return Optional.of(out.toByteArray()[0]);
     }
     /**
-     * Converts an integer to a hex formatted string
-     * 
-     * @param decimal The number to convert
-     * @param bytes   The amount of bytes expected
-     * @return String starting with 0x and followed by even amount of uppercase
-     *         hexadecimal values
-     */
-    public static String fromDecToHexString(int decimal, int bytes) {
-        StringBuilder hex = new StringBuilder(Integer.toHexString(decimal));
-
-        while (hex.length() % 2 == 1 || hex.length() / 2 < bytes)
-            hex.insert(0, "0");
-        return "0x" + hex.toString().toUpperCase();
-    }
-
-    /**
-     * Converts and integer smaller than 256 to a hex formatted string
-     * 
-     * @param decimal The decimal to convert
-     * @return String starting with 0x and followed by even amount of uppercase
-     *         hexadecimal values
-     */
-    public static String fromDecToHexString(int decimal) {
-        return fromDecToHexString(decimal, 1);
-    }
-
-    public static int[] fromBytesToUnsigned(byte[] bytes, int offset, int length) {
-        int[] ints = new int[length];
-        length+=offset;
-        for (int a = offset; a < bytes.length && a < length; a++) {
-            int x = bytes[a];
-            ints[a-offset] = x < 0 ? x + 256 : x;
-        }
-        return ints;
-    }
-
-    public static String appendNMEAChecksum(String nmea) {
-		int checksum = 0;
-		for (int i = 1; i < nmea.length(); i++) {
-			checksum = checksum ^ nmea.charAt(i);
-		}
-		return nmea + "*" + Integer.toHexString(checksum).toUpperCase();
-	}
-
-    /**
      * Converts meters to kilometers with the given amount of decimals
      * @param m The amount of meters
      * @param decimals The amount of decimals
@@ -543,80 +394,7 @@ public class Tools {
             return roundDouble(m / 1000, 1) + "km";
         return roundDouble(m, decimals) + "m";
     }
-
-    /**
-     * Converts meters to feet with an specified amount of decimals
-     * @param m The amount of meters
-     * @param decimals The amount of decimals
-     * @return The formatted result
-     */
-    public static double metersToFeet(double m, int decimals) {
-        return roundDouble(m * 3.2808399, decimals);
-    }
-
-    /**
-     * Converts meters to fathoms with an specified amount of decimals
-     * @param m The amount of meters
-     * @param decimals The amount of decimals
-     * @return The formatted result
-     */
-    public static double metersToFathoms(double m, int decimals) {
-        return roundDouble(m * 0.546806649, decimals);
-    }
-
     /* ***************************************** * O T H E R *************************************************** */
-
-    /**
-     * Determine the beaufort scale according to an average windvelocity
-     * 
-     * @param minuteAverageWindSpeed Averaged windvelocity
-     * @return Current Beaufort level
-     */
-    public int getBeaufortNr(double minuteAverageWindSpeed) {
-        int bf = 0;
-        if (minuteAverageWindSpeed < 0.2) {
-            bf = 0;
-            // beaufortDescr = "Calm";
-        } else if (minuteAverageWindSpeed < 1.5) {
-            bf = 1;
-            // beaufortDescr = "Light Air";
-        } else if (minuteAverageWindSpeed < 3.3) {
-            bf = 2;
-            // beaufortDescr = "Light Breeze";
-        } else if (minuteAverageWindSpeed < 5.4) {
-            bf = 3;
-            // beaufortDescr = "Gentle Breeze";
-        } else if (minuteAverageWindSpeed < 7.9) {
-            bf = 4;
-            // beaufortDescr = "Moderate Breeze";
-        } else if (minuteAverageWindSpeed < 10.7) {
-            bf = 5;
-            // beaufortDescr = "Fresh Breeze";
-        } else if (minuteAverageWindSpeed < 13.8) {
-            bf = 6;
-            // beaufortDescr = "Strong Breeze";
-        } else if (minuteAverageWindSpeed < 17.1) {
-            bf = 7;
-            // beaufortDescr = "High Wind, Moderate Gale";
-        } else if (minuteAverageWindSpeed < 20.7) {
-            bf = 8;
-            // beaufortDescr = "Gale";
-        } else if (minuteAverageWindSpeed < 24.4) {
-            bf = 9;
-            // beaufortDescr = "Strong Gale";
-        } else if (minuteAverageWindSpeed < 28.4) {
-            bf = 10;
-            // beaufortDescr = "Storm";
-        } else if (minuteAverageWindSpeed < 32.6) {
-            bf = 11;
-            // beaufortDescr = "Violent Storm";
-        } else {
-            bf = 12;
-            // beaufortDescr = "Hurricane";
-        }
-        return bf;
-    }
-
     /**
      * Retrieve the MAC address of an network interface based on the displayname
      * 
@@ -633,7 +411,6 @@ public class Tools {
                     continue;
                 String mac = Tools.fromBytesToHexString(iface.getHardwareAddress()).replace(" ", ":");
                 mac = mac.replace("0x", "");
-                // Logger.info(" MAC for "+iface.getDisplayName()+" -> "+mac);
                 if (iface.getDisplayName().equalsIgnoreCase(displayname))
                     return mac;
             }
@@ -644,15 +421,15 @@ public class Tools {
     }
 
     /**
-     * Retrieve the IP address of an network interface based on the displayname. If
-     * displayname is "" then all info from all interfaces will be returned
+     * Retrieve the IP address of an network interface based on the displayName. If
+     * displayName is "" then all info from all interfaces will be returned
      * including mac address
      * 
-     * @param displayname The name of the interface fe. wlan0
+     * @param displayName The name of the interface fe. wlan0
      * @param ipv4        True if the IPv4 is wanted or false for IPv6
      * @return The found IP or empty string if not found
      */
-    public static String getIP(String displayname, boolean ipv4) {
+    public static String getIP(String displayName, boolean ipv4) {
         StringJoiner join = new StringJoiner("\r\n");
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
@@ -666,13 +443,13 @@ public class Tools {
                 while (addresses.hasMoreElements()) {
                     InetAddress addr = addresses.nextElement();
                     String p = addr.getHostAddress();
-                    if (displayname.isEmpty()) {
+                    if (displayName.isEmpty()) {
                         String mac = Tools.fromBytesToHexString(iface.getHardwareAddress()).replace(" ", ":");
                         mac = mac.replace("0x", "");
                         if ((p.contains(":") && !ipv4) || (p.contains(".") && ipv4))
                             join.add(iface.getDisplayName() + " -> " + p + " [" + mac + "]");
                     } else {
-                        if (iface.getDisplayName().equalsIgnoreCase(displayname)) {
+                        if (iface.getDisplayName().equalsIgnoreCase(displayName)) {
                             if ((p.contains(":") && !ipv4) || (p.contains(".") && ipv4))
                                 return p;
                         }
@@ -705,13 +482,6 @@ public class Tools {
             pairs.forEach(p -> splits.add(p.split(":")));
         }
         return splits;
-    }
-    public static List<String[]> parseKeyValueNoBrackets( String data ){
-        return Arrays.stream(data.split(" ")) // simple split on spaces
-                        .distinct() // remove duplicates
-                        .filter(w -> !w.startsWith("{")&&!w.contains(":")) // igore those that start with { or don't contain :
-                        .map( w -> new String[]{ w.substring(0,w.indexOf(":")),w.substring(w.indexOf(":")+1)})
-                        .collect(Collectors.toList()); // gather them in a list
     }
     public static String parseExpression( String op ){
         op=op.replace("->"," through");
@@ -749,6 +519,11 @@ public class Tools {
 
         return op.replace(" ","");
     }
+
+    /**
+     * Get the IP of this system.
+     * @return IP of this system.
+     */
     public static String getLocalIP() {
         try (final DatagramSocket socket = new DatagramSocket()) {
             var sock = InetAddress.getByName("8.8.8.8");
@@ -760,7 +535,7 @@ public class Tools {
         }
     }
     /**
-     * Execute commands associated with serialports on the system
+     * Execute commands associated with serial ports on the system
      *
      * @param html Whether to use html for newline etc
      * @return Descriptive result of the command, "Unknown command if not recognised
