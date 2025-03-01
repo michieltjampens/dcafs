53e795264b2b5a8bd24081b74bf579fac10bafbd
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2025-03-01 16:55:10 +0100

    cleanup

--- uninstall_dcafs_service.sh
+++ uninstall_dcafs_service.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
+# Stop and disable the service
 sudo systemctl stop dcafs.service
 sudo systemctl disable dcafs.service
-sudo  rm -f /lib/systemd/system/dcafs.service
+
+# Remove the service definition
+sudo rm -f /lib/systemd/system/dcafs.service
+
+# Reload the systemd manager configuration
 sudo systemctl daemon-reload
+
+# Reset the failed state of the service
 sudo systemctl reset-failed
 
