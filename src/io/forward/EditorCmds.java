f2c66b16a602653a960f53530fd72edc75354c5a
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2025-03-03 22:11:43 +0100

    rewrote to use flow style to build the node

--- src/io/forward/EditorCmds.java
+++ src/io/forward/EditorCmds.java
@@ -3,171 +3,144 @@ package io.forward;
 import util.xml.XMLfab;
 
 public class EditorCmds {
-    private static XMLfab addNode( XMLfab fab, String type, String value, String comment){
-        if( !comment.isEmpty())
-            fab.comment(comment);
-        fab.addChild(type,value);
-        return fab;
-    }
-    public static String addEditor(XMLfab fab, String type, String value ){
-        String deli="";
+
+    public static String addEditor(XMLfab fab, String type, String value) {
+
+        NodeAdder node = NodeAdder.withFab(fab).value(value).type(type);
+
         String[] p = value.split("[|]");
-        switch (type) {
+        return switch (type) {
             /* Splitting */
-            case "rexsplit" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,rexsplit,delimiter|regextomatch";
-                if (value.startsWith(",")) {
-                    deli = ",";
-                    value = value.substring(2);
-                } else {
-                    int in = value.indexOf(",");
-                    deli = value.substring(0, in);
-                    value = value.substring(in + 1);
-                }
-                addNode( fab,type,value,"Find matches on " + value + " then concatenate with " + deli)
-                        .attr("type","rexsplit");
-                return "Find matches on " + value + " then concatenate with " + deli;
-            }
-            case "resplit" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,resplit,delimiter|format";
-                if (value.startsWith(",")) {
-                    deli = ",";
-                    value = value.substring(2);
-                } else {
-                    int in = value.indexOf(",");
-                    deli = value.substring(0, in);
-                    value = value.substring(in + 1);
-                }
-                addNode( fab,type,value,"Split on " + deli + " then combine according to " + value)
-                        .attr("delimiter", deli)
-                        .attr("leftover", "append")
-                        .build();
-                return "Split on '" + deli + "', then combine according to " + value + " and append leftover data";
-            }
-            case "charsplit" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,rexplit,regextosplitwith";
-
-                addNode(fab, type,value,"");
-                return "Charsplit added with default delimiter";
-            }
+            case "rexsplit", "resplit" -> doSplit(fab, type, value);
+            case "charsplit" -> node.args("regextosplitwith")
+                    .comment("Charsplit added with default delimiter").build();
             /* Timestamp stuff */
-            case "redate" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,redate,index|from|to";
-
-                addNode(fab, "redate", p[2], "")
-                    .attr("index", p[0])
+            case "redate" -> node.args("index|from|to").value(p[2]).attr("index", p[0])
+                    .comment("The date on index " + p[0] + " is reformatted from " + p[1] + " to " + p[2])
                     .build();
-                return "After splitting on " + deli + " the date on index " + p[0] + " is reformatted from " + p[1] + " to " + p[2];
-            }
-            case "retime" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addeditor,retime,index|from|to";
-
-                addNode(fab, "retime", p[2], "")
-                    .attr("index", p[1])
+            case "retime" -> node.args("index|from|to").value(p[2]).attr("index", p[1])
+                    .comment("Time on index " + p[1] + " is reformatted from " + p[0] + " to " + p[2])
                     .build();
-                return "After splitting on , the time on index " + p[1] + " is reformatted from " + p[0] + " to " + p[2];
-            }
             /* Replacing */
-            case "replace" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,replace,find|replacement";
-                addNode( fab, type,p[1],"Replace " + p[0] + " with " + p[1])
-                        .attr("find", p[0])
-                        .build();
-                return "Replacing " +  p[0] + " with " + p[1];
-            }
-            case "rexreplace" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,rexreplace,regexwhat|replacement";
-                addNode( fab, type,value,"RexReplace " + p[0] + " with " + p[1])
-                        .attr("find", p[0])
-                        .build();
-                return "Replacing " + p[0] + " with " + p[1];
-            }
-            case "replaceindex","indexreplace" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,"+type+",index|replacement";
-                addNode( fab, type,value,type+" " + p[0] + " with " + p[1])
-                        .attr("find", p[0])
-                        .build();
-                return "Replacing value at " + p[0] + " with " + p[1];
-            }
+            case "replace" -> node.args("find|replacement")
+                    .value(p[1]).attr("find", p[0])
+                    .comment("Replace " + p[0] + " with " + p[1]).build();
+            case "rexreplace" -> node.args("regexwhat|replacement")
+                    .attr("find", p[0])
+                    .comment("Replacing " + p[0] + " with " + p[1]).build();
+            case "replaceindex", "indexreplace" ->
+                    node.args("index|replacement").comment("Replacing value at " + p[0] + " with " + p[1]).build();
             /* Remove stuff */
-            case "remove" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,remove,find";
-                addNode( fab, type,"","Removing " + value+" from data")
-                        .attr("find", p[0])
-                        .build();
-                return "Removing " + value + " from the data";
-            }
-            case "rexremove" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,rexremove,regexfind";
-                addNode( fab, type,"","Removing " + value+" from data")
-                        .attr("find", p[0])
-                        .build();
-                return "Removing matches of " + value + " from the data";
-            }
-            case "trim" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,trim";
-                addNode( fab, type,"","Trimming data")
-                        .build();
-                return "Trimming spaces from data";
-            }
-            case "cutstart" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,cutstart,charcount";
-                addNode( fab, type,value,"Cutting "+value+" chars from the start")
-                    .build();
-                return "Cutting " + value + " char(s) from the start";
-            }
-            case "cutend" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,cutend:charcount";
-                addNode( fab, type,value,"Cutting "+value+" chars from the end")
-                        .build();
-                return "Cutting " + value + " char(s) from the end";
-            }
-            case "removeindex" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,removeindex:index";
-                addNode( fab, type,value,"Removing item at index "+value)
-                        .build();
-                return "Removing item at index "+value;
-            }
+            case "remove" ->
+                    node.args("find").value("").attr("find", p[0]).comment("Removing " + value + " from the data").build();
+            case "rexremove" ->
+                    node.args("regexfind").value("").attr("find", p[0]).comment("Removing " + value + " from data").build();
+            case "trim" -> node.value("").comment("Trimming spaces from data").build();
+            case "cutstart" -> node.args("charcount").comment("Cutting " + value + " chars from the start").build();
+            case "cutend" -> node.args("charcount").comment("Cutting " + value + " chars from the end").build();
+
+            case "removeindex" -> node.args("index").comment("Removing item at index " + value).build();
             /* Adding stuff */
-            case "prepend", "prefix" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,prepend:toprepend or pf:pathid,addedit,prefix:toprepend";
-                addNode( fab, type,value,"Prepending " + value + " to the data")
-                        .build();
-                return "Prepending " + value + " to the data";
-            }
-            case "insert" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,insert:position,toinsert";
-                var what = value.substring(value.indexOf(",")+1);
-                var where = value.substring(0,value.indexOf(","));
-                addNode( fab, type,what,"Inserting " + what + " at char " + where + " in the data")
-                        .attr("position", where)
-                        .build();
-                return "Inserting " + value + " at char " + p[0] + " in the data";
-            }
-            case "append", "suffix" -> {
-                if (value.equals("?"))
-                    return "pf:pathid,addedit,append:toappend or pf:pathid,addedit,suffix:toappend";
-                addNode( fab, type,value,"Appending " + value + "to the data")
-                        .build();
-                return "Appending " + value + " to the data";
-            }
+            case "prepend", "prefix" ->
+                    node.args(type + ":toprepend").comment("Prepending " + value + " to the data").build();
+            case "insert" -> node.args(":position,toinsert")
+                    .value(value.substring(value.indexOf(",") + 1))
+                    .attr("position", value.substring(0, value.indexOf(",")))
+                    .comment("Inserting " + node.value() + " at char " + node.attrValue() + " in the data").build();
+            case "append", "suffix" ->
+                    node.args(type + ":toappend").comment("Appending " + value + " to the data").build();
+            default -> "Unknown type " + type;
+        };
+    }
+
+
+    private static String doSplit( XMLfab fab, String type, String value ){
+        String deli;
+        if (value.startsWith(",")) {
+            deli = ",";
+            value = value.substring(2);
+        } else {
+            int in = value.indexOf(",");
+            deli = value.substring(0, in);
+            value = value.substring(in + 1);
         }
-        return "unknown type: "+type;
+        String comment="";
+        if( type.equals("rexsplit")){
+            comment="Find matches on " + value + " then concatenate with " + deli;
+        }else if( type.equals("resplit")){
+            comment="Split on " + deli + " then combine according to " + value;
+        }
+        fab.comment(comment);
+        fab.addChild(type,value);
+        if( type.equals("rexsplit")){
+            fab.attr("type","rexsplit");
+        }else if( type.equals("resplit")){
+            fab.attr("delimiter", deli)
+               .attr("leftover", "append");
+        }
+        fab.build();
+        return comment;
+    }
+    static class NodeAdder {
+        private final XMLfab fab;
+        private String type;
+        private String value;
+        private String args = "";
+        private String comment;
+        private String[] attr;
+
+        public NodeAdder(XMLfab fab) {
+            this.fab = fab;
+        }
+
+        public static NodeAdder withFab(XMLfab fab) {
+            return new NodeAdder(fab);
+        }
+
+        public NodeAdder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public NodeAdder value(String value) {
+            this.value = value;
+            return this;
+        }
+
+        public String value() {
+            return value;
+        }
+
+        public NodeAdder args(String args) {
+            this.args = args;
+            return this;
+        }
+
+        public NodeAdder comment(String comment) {
+            this.comment = comment;
+            return this;
+        }
+
+        public NodeAdder attr(String att, String val) {
+            attr = new String[]{att, val};
+            return this;
+        }
+
+        public String attrValue() {
+            return attr[1];
+        }
+
+        public String build() {
+            if (value.equals("?"))
+                return "pf:pathid,addedit," + type + (args.isEmpty() ? "" : ",") + args;
+
+            if (!comment.isEmpty())
+                fab.comment(comment);
+            fab.addChild(type, value);
+            if (attr != null)
+                fab.attr(attr[0], attr[1]);
+            fab.build();
+            return comment;
+        }
     }
 }
