b9104ca37d893dfb130abf6813b1efcd65361709
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2025-04-17 03:12:17 +0200

    A step to easily determine what happens at the end

---
+++ src/io/forward/ReturnStep.java
@@ -0,0 +1,16 @@
+package io.forward;
+
+import org.tinylog.Logger;
+
+import java.math.BigDecimal;
+
+public class ReturnStep extends AbstractStep {
+    @Override
+    public String takeStep(String data, BigDecimal[] bds) {
+        return data;
+    }
+
+    public void setNext(AbstractStep next) {
+        Logger.info("Ignoring setNext on return");
+    }
+}
