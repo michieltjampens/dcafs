0bca498652aac9b8c5f905f5a811cdf833aa3aeb
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2025-05-18 16:45:38 +0200

    rewrote so it isn't limited to origin blocks

--- src/util/tasks/blocks/ControlBlock.java
+++ src/util/tasks/blocks/ControlBlock.java
@@ -4,21 +4,32 @@ import io.netty.channel.EventLoopGroup;
 
 public class ControlBlock extends AbstractBlock {
     EventLoopGroup eventLoop;
-    OriginBlock start;
-    OriginBlock end;
+    AbstractBlock start;
+    AbstractBlock stop;
 
-    public ControlBlock(EventLoopGroup eventLoop, OriginBlock start, OriginBlock end) {
+    public ControlBlock(EventLoopGroup eventLoop, AbstractBlock start, AbstractBlock stop) {
         this.eventLoop = eventLoop;
         this.start = start;
-        this.end = end;
+        this.stop = stop;
     }
 
+    public void setBlocks(AbstractBlock start, AbstractBlock stop) {
+        if (start != null)
+            this.start = start;
+        if (stop != null)
+            this.stop = stop;
+    }
     @Override
     public boolean start() {
-        if (start != null)
-            eventLoop.submit(start::restart);
-        if (end != null)
-            eventLoop.submit(end::reset);
+        if (start != null) {
+            if (start instanceof OriginBlock ori) {
+                eventLoop.submit(ori::restart);
+            } else {
+                eventLoop.submit(start::start);
+            }
+        }
+        if (stop != null)
+            eventLoop.submit(stop::reset);
         doNext();
         return true;
     }
